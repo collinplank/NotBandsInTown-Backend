41506720de224b06ccc29cd4d0199e1a1b40fd1f
Collin Plank <collin.plank@gmail.com>
2024-12-21 11:11:05 -0600

    Add user signup action

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  post "/users" => "users#create"
+
   get "/artists" => "artists#index"
   post "/artists" => "artists#create"
   get "/artists/:id" => "artists#show"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_09_213327) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_21_170515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,6 +34,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_09_213327) do
   create_table "setlists", force: :cascade do |t|
     t.integer "concert_id"
     t.string "songs"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
