48abf84abf7cd4ffe6bbb2a0cb9cce449bca81ef
Collin Plank <collin.plank@gmail.com>
2025-01-07 15:49:22 -0500

    Update Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_21_170515) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_07_192628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_21_170515) do
     t.string "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "concerts", force: :cascade do |t|
